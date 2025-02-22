c8efe75ebde668ccd400b3bdcf5f71fe82ff5495
Gabe Lopez <git.unfixed408@passmail.net>
2025-02-22 06:47:10 -0600

    added Comment, Post Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,31 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $users = User::factory(10)->create();
+        $posts = Post::factory(200)->recycle($users)->create();
+        $comments = Comment::factory(100)->recycle($users)->recycle($posts)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+
+        $gabe = User::factory()
+            ->has(Post::factory(45))
+            ->has(Comment::factory(120)->recycle($posts))
+            ->create([
+                'name' => 'Gabe',
+                'email' => 'test@example.com',
+            ]);
     }
 }
